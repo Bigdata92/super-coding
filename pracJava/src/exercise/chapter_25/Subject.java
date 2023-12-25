51a8f1cb97df8c58acbdf8e4a83bd552acbba328
JiseopSong <wltkqdl@gmail.com>
2023-12-25 12:52:17 +0900

    25강

---
+++ pracJava/src/exercise/chapter_25/Subject.java
@@ -0,0 +1,7 @@
+package exercise.chapter_25;
+
+public class Subject {
+
+    String testSubjectName;
+    String testSubjectCode;
+}
---
+++ pracJava/src/exercise/chapter_25/Teacher.java
@@ -0,0 +1,21 @@
+package exercise.chapter_25;
+
+public class Teacher {
+
+    // 학교 정보
+    String schoolName;
+
+    // 과목
+    Subject subject;
+
+    // 기본 정보
+    String name;
+    String gender;
+
+    // 행위
+    void teach(Student student, String subjectName) {
+        // TODO : 나중에 구현
+        String studentName = student.name;
+        System.out.printf("선생님 : 저는 %s 에게 %s 과목을 가르치고 있습니다.", studentName, subjectName);
+    }
+}
